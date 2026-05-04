e49924e4bcb4feece2bc632c2c5022046bf3b791
rachiidboy-a11y <rachiidboy@gmail.com>
2026-05-04 16:49:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.get("/proxy", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Proxy running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Proxy running on port " + PORT);
 });
